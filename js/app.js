ca5c533c6df53ae3047699b1cb1afc9f011cd573
Tanz Sullamora <dabldee@gmail.com>
2013-07-09 12:16:28 +0400

    Small fix for coasts on bridges

--- js/app.js
+++ js/app.js
@@ -75,28 +75,28 @@ $(function() {
 			for (var j = offset[0]; j < offset[0] + WIDTH; j++) {
 				if (levels[0].map[i] != undefined && levels[0].map[i][j] != undefined) {
 					if (levels[0].map[i][j] == 'water') {
-						if (levels[0].map[i][j - 1] != undefined && levels[0].map[i][j - 1] != 'water') {
+						if (levels[0].map[i][j - 1] != undefined && levels[0].map[i][j - 1] != 'water' && levels[0].map[i][j - 1] != 'bridge') {
 							context.save();
 							context.translate((j - offset[0] + 1) * 30 - 15, (i - offset[1] + 1) * 30 - 15);
 							context.rotate(1.56);
 							context.drawImage(getBlockSprite('coast'), -15, -15);
 							context.restore();
 						}
-						if (levels[0].map[i][j + 1] != undefined && levels[0].map[i][j + 1] != 'water') {
+						if (levels[0].map[i][j + 1] != undefined && levels[0].map[i][j + 1] != 'water' && levels[0].map[i][j + 1] != 'bridge') {
 							context.save();
 							context.translate((j - offset[0] + 1) * 30 - 15, (i - offset[1] + 1) * 30 - 15);
 							context.rotate(4.7);
 							context.drawImage(getBlockSprite('coast'), -15, -15);
 							context.restore();
 						}
-						if (levels[0].map[i - 1] != undefined && levels[0].map[i - 1][j] != undefined && levels[0].map[i - 1][j] != 'water') {
+						if (levels[0].map[i - 1] != undefined && levels[0].map[i - 1][j] != undefined && levels[0].map[i - 1][j] != 'water' && levels[0].map[i - 1][j] != 'bridge') {
 							context.save();
 							context.translate((j - offset[0] + 1) * 30 - 15, (i - offset[1] + 1) * 30 - 15);
 							context.rotate(3.13);
 							context.drawImage(getBlockSprite('coast'), -15, -15);
 							context.restore();
 						}
-						if (levels[0].map[i + 1] != undefined && levels[0].map[i + 1][j] != undefined && levels[0].map[i + 1][j] != 'water') {
+						if (levels[0].map[i + 1] != undefined && levels[0].map[i + 1][j] != undefined && levels[0].map[i + 1][j] != 'water' && levels[0].map[i + 1][j] != 'bridge') {
 							context.save();
 							context.drawImage(getBlockSprite('coast'), (j - offset[0]) * 30, (i - offset[1]) * 30);
 							context.restore();
